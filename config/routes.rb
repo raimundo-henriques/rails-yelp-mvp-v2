30af3f2dc0e7b76e11c6ecb59824cd367c50c7b6
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-08 09:24:00 +0200

    TA prep: restaurants#index done (barely any front-end)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get 'restaurants/index'
+  # get 'restaurants/show'
+  # get 'restaurants/new'
+  # get 'restaurants/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [ :index, :new, :create, :show ]
 end
